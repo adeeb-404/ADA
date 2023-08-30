3243c522c4905d54518ec4462bfb8534090f0303
Adeeb <116057031+adeeb-404@users.noreply.github.com>
2023-08-30 23:40:32 +0530

    Create Horsepool.cpp

---
+++ Horsepool.cpp
@@ -0,0 +1,24 @@
+    // string matching
+    
+    string text = "raijraj";
+    string pattern = "raj";
+    int n = text.size();
+    int m = pattern.size();
+    vector<int> hash(27,m);
+    
+    for(int i=0;i<m-1;i++)
+        hash[pattern[i]-'a'] = m-1-i;
+        
+    //horse
+    int i=m-1;
+    while(i<n){
+        int k=0;
+        while(k<m&&pattern[m-k-1]==text[i-k])
+            k++;
+        if(k==m){
+            cout<<"Matched at index "<<i-k+1<<endl;
+            break;
+        }
+        else    i+= hash[text[i]-'a'];
+    }
+    
