e6abeedd9c39285fe585edb504bdceb2c9fe872f
Adeeb <116057031+adeeb-404@users.noreply.github.com>
2023-08-30 23:34:55 +0530

    Create NQueens.cpp

---
+++ NQueens.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<string>> ans;
+vector<bool> fd,bd,c;
+bool valid(vector<string> &grid,int row,int col,int n){
+    // for(int i =0;i<row;i++){
+    //     if(grid[i][col]=='Q')   return false;
+    // }
+    
+    // int i = row,j = col;
+    // while(i>=0&&j>=0){
+    //     if(grid[i][j]=='Q') return false;
+    //     i--,j--;
+    // }
+    // i = row,j =col;
+    // while(i>=0&&j<n){
+    //     if(grid[i][j]=='Q') return false;
+    //     i--,j++;
+    // }
+    
+    // return true;
+    
+    return fd[row+col]==false&&bd[row-col+n-1]==false&&c[col]==false;
+    
+}
+
+void solve(vector<string> &grid,int row,int n){
+    
+    if(row==n){
+        ans.push_back(grid);
+        return;
+    }
+    
+    for(int i=0;i<n;i++){
+        if(valid(grid,row,i,n)){
+            fd[row+i]=true;
+            bd[row-i+n-1]=true;
+            c[i]=true;
+            grid[row][i]='Q';
+            solve(grid,row+1,n);
+            grid[row][i]='.';
+            fd[row+i]=false;
+            bd[row-i+n-1]=false;
+            c[i]=false;
+        }
+    }
+}
+
+
+int main()
+{
+    
+    int n;
+    cout<<"Enter N of N-Queens\n"<<endl;
+    cin>>n;
+    fd.resize(2*n-1,false);
+    bd.resize(2*n-1,false);
+    c.resize(n,false);
+    
+    vector<string> grid(n);
+    for(int i=0;i<n;i++){
+        string s="";
+        for(int j=0;j<n;j++){
+            s +='.';
+        }
+        grid[i]=s;
+    }
+    
+    solve(grid,0,n);
+    if(ans.size()==0)   cout<<"Not possible\n";
+    else{
+    cout<<"The valid combinations are:\n";
+    
+    for(auto g : ans){
+        for(auto r : g){
+            for(auto c : r){
+                cout<<c<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<"\n\n";
+    }
+}
+    return 0;
+}
+
+
